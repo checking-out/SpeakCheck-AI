5b3e174ba6d7b7d129c76ce4d381e07148a645c9
123isi <123isi@naver.com>
2025-11-13 19:49:34 +0900

    ai했다

--- api.py
+++ api.py
@@ -120,7 +120,7 @@ question_generator = QuestionGenerator()
 app = FastAPI(title="SpeakCheck Whisper API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
